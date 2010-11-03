a72fc490a4c6b26f7a9e232f5816eec4bb3627f9
Philipp Bosch <pb@philippbosch.de>
2010-11-03 23:40:33 +0100

    Reload by device turn hack added.

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ $.jQTouch({
     statusBar: 'default',
     useTouch: false
 });
+
+$(document).ready(function() {
+    // Reload the app when device is turned left and back right. Useful if run in fullscreen mode in simulator.
+    $('body').bind('turn', function(e, info) {
+        if (info.orientation == "portrait") window.location.reload();
+    });
+});
