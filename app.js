0bb485c81500b9ad684bd0f7f9ca4cd3e370cf35
Philipp Bosch <pb@philippbosch.de>
2010-11-04 01:46:57 +0100

    Simple geolocation example with static map added.

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ $.jQTouch({
 });
 
 $(document).ready(function() {
+
+    // Detect the current position of the device
+    function success(position) {
+        $('#position').html('Latitude: ' + position.coords.latitude + '<br>Longitude: ' + position.coords.longitude);
+        var mapUrl = 'http://maps.google.com/maps/api/staticmap?center=' + position.coords.latitude + ',' + position.coords.longitude + '&zoom=16&size=320x352&maptype=roadmap&markers=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=false';
+        $('#map').html('<img src="' + mapUrl + '">');
+    }
+    function error(message) {
+        $('#position').text("Position kann nicht ermittelt werden.");
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+    
     // Reload the app when device is turned left and back right. Useful if run in fullscreen mode in simulator.
     $('body').bind('turn', function(e, info) {
         if (info.orientation == "portrait") window.location.reload();
